5660fba9105d5eb2c6b52c7aa357c46c38883c88
TheStackMaster <unijoy84@gmail.com>
2025-05-22 21:19:04 +0600

    ready for final submission

--- src/Layouts/MainLayouts.jsx
+++ src/Layouts/MainLayouts.jsx
@@ -23,14 +23,3 @@ const MainLayouts = () => {
 
 export default MainLayouts;
 
-// #006838
-// #40b93c
-
-// https://i.ibb.co/ppDpRDj/money-Plant.png
-// https://i.ibb.co/Y7XcvF6r/Bonsai.jpg
-// https://i.ibb.co/wZJKymfg/Bamboo-Palm.jpg
-// https://i.ibb.co/4RWK0Yk7/snake-Plant.png
-// https://i.ibb.co/390yCpZ5/peach-Lily.png
-// https://i.ibb.co/0y3fX89G/Hibiscus.jpg
-// https://i.ibb.co/LdQMcmwr/bsoton-fern.jpg
-// https://i.ibb.co/1Jn4Z5x8/Aloe-vera.jpg
